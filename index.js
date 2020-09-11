cf9b07d19fa596a0d57b818f124e0770b0a39e61
Prasanna Subramaniam <prasannatiruppur@gmail.com>
2020-09-11 16:25:40 +0530

    auth completed

--- index.js
+++ index.js
@@ -1,62 +1,102 @@
 const express = require('express')
 const cors = require('cors')
+const jwt = require("jsonwebtoken")
+const bcrypt = require("bcryptjs")
+const bodyParser = require("body-parser")
 
-// const myDB=require('./data.json');
+//PROT
+const port = process.env.PORT || 3000;
 
+//MongoDB initialization
 const MongoClient = require('mongodb').MongoClient
-let url="";
-if(process.env.URL){
-    url=process.env.URL;
-}
-else{
-    url='mongodb://127.0.0.1:27017';
-    console.log(url);
-}
+const url=process.env.URL || 'mongodb://127.0.0.1:27017';
 const dbName = 'swiggy-replica'
-
 let mongodb;
 MongoClient.connect(url, {  
-    poolSize: 10
-  },function(err, db) {
-      mongodb=db;
-      }
-  );
+        poolSize: 10,
+        useUnifiedTopology: true
+    },function(err, db) {
+            mongodb=db;
+        }
+    );
 
-//-------Template---------
-// MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
-//     if (err) return console.log(err)
-//     db = client.db(dbName)
-//     col=db.collection('------');
-//     col.find({}).toArray(function(err,docs){
-//         let data={};
-//         res.json(data);
-//     });
-// })
-//-------------------------
-
-// const MongoClient = require('mongodb').MongoClient;
-// const uri = "mongodb+srv://<name>:<pwd>@prasanna.6bywy.gcp.mongodb.net/swiggy-replica?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useUnifiedTopology: true });
-// client.connect(err => {
-//     const collection = client.db("swiggy-replica").collection("offers");
-//     console.log(collection.find({}));
-//     client.close();
-// });
+//bcrypt and JWT initialization
+const saltRounds=8;
+const secret = process.env.secret || 'helloworld';
+verifyToken = (req, res, next) => {
+    let token = req.headers["x-access-token"];
+    if (!token) {
+        return res.status(403).send({ message: "No token provided!" });
+    }
+    jwt.verify(token, secret, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ message: "Unauthorized!" });
+        }
+        req.userId = decoded.id;
+        req.userName = decoded.name;
+        req.userEmail = decoded.email;
+        next();
+    });
+};
 
 const app=express();
-app.use(cors())
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/api/offers',(req,res)=>{
+app.post('/api/auth/signup',(req,res)=>{
+    // console.log(req.body);
+    bcrypt.hash(req.body.password, saltRounds).then(function(hash) {
+        col = mongodb.db(dbName).collection('user');
+        col.findOne({email:req.body.email},(err,data)=>{
+            if(!data){
+                col.insertOne({name:req.body.name,email:req.body.email,password:hash}, function(err, data) {
+                    if (!err)
+                        res.json({"userInserted":true,"message":"Account created successfully!! Please Login"})
+                });
+            }
+            else{
+                res.json({"userInserted":false,"message":"Email ID already exists please try login!!"})
+            }
+        })
+    });
+})
+
+app.post('/api/auth/login',(req,res)=>{
+    col = mongodb.db(dbName).collection('user');
+    col.findOne({email:req.body.email},(err,data)=>{
+        if(data){
+            bcrypt.compare(req.body.password, data.password).then(function(result) {
+                if(result){
+                    var token = jwt.sign({ id: data._id,name:data.name,email:data.email },secret, {
+                        expiresIn: 86400 // 24 hours
+                    });
+                    // console.log(token)
+                    res.json({"loggedin":true,"message":"user loggedin successfully","token":token})
+                }
+                else{
+                    res.json({"loggedin":false,"message":"Incorrect Email ID or Password!!"})
+                }
+            });
+        }
+        else{
+            res.json({"loggedin":false,"message":"Incorrect Email ID or Password!!"})
+        }
+    })
+})
+
+app.get('/api/offers',verifyToken,(req,res)=>{
     col = mongodb.db(dbName).collection('offers');
     col.find({}).toArray(function(err,docs){
         let data={
             offers:docs[0].offers
         };
-        res.json(data);
+        // res.json(data);
+        res.status(200).send(data);
     });
 });
 
-app.get('/api/restaurants',(req,res)=>{
+app.get('/api/restaurants',verifyToken,(req,res)=>{
     col = mongodb.db(dbName).collection('restaurants');
     col.find({}).toArray((err,docs)=>{
         let data={
@@ -101,11 +141,12 @@ app.get('/api/restaurants',(req,res)=>{
             }
             data.restaurantsList.push(temp)
         }
-        res.json(data);
+        // res.json(data);
+        res.status(200).send(data);
     })
 });
 
-app.get('/api/restaurant/:id',(req,res)=>{
+app.get('/api/restaurant/:id',verifyToken,(req,res)=>{
     col = mongodb.db(dbName).collection('restaurants');
     col.find({"id":req.params.id}).toArray(function(err,docs){
         docs=docs[0];
@@ -137,10 +178,33 @@ app.get('/api/restaurant/:id',(req,res)=>{
             menuCategory,
             recommended:items
         }
-        res.json(result);
+        // res.json(result);
+        res.status(200).send(result);
     });
 })
 
-app.listen(process.env.PORT || 3000,()=>{
-    console.log(`Process running on port ${process.env.PORT || 3000}`)
+app.listen(port,()=>{
+    console.log(`Process running on port ${port}`)
 });
+
+
+//-------Template---------
+// MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
+//     if (err) return console.log(err)
+//     db = client.db(dbName)
+//     col=db.collection('------');
+//     col.find({}).toArray(function(err,docs){
+//         let data={};
+//         res.json(data);
+//     });
+// })
+//-------------------------
+
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://<name>:<pwd>@prasanna.6bywy.gcp.mongodb.net/swiggy-replica?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useUnifiedTopology: true });
+// client.connect(err => {
+//     const collection = client.db("swiggy-replica").collection("offers");
+//     console.log(collection.find({}));
+//     client.close();
+// });
