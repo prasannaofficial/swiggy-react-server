4ddee8c821590516e5d2fbda993615b015e19feb
Prasanna Subramaniam <prasannatiruppur@gmail.com>
2020-11-02 16:53:02 +0530

    Push notification done

--- firebaseInit.js
+++ firebaseInit.js
@@ -4,6 +4,7 @@ const admin = require("firebase-admin");
 
 // const serviceAccount = require(googleApplicationCredentials);
 const { GOOGLE_APPLICATION_CREDENTIALS } = require("./config/keys");
+console.log(GOOGLE_APPLICATION_CREDENTIALS);
 admin.initializeApp({
   credential: admin.credential.cert(GOOGLE_APPLICATION_CREDENTIALS),
   databaseURL: "https://fir-cloud-messaging-c7269.firebaseio.com",
