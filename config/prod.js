bad2216ff34943bf7229ebfba4fbb4e39e9c6483
Prasanna Subramaniam <prasannatiruppur@gmail.com>
2020-11-02 16:49:48 +0530

    Push notification done

--- config/prod.js
+++ config/prod.js
@@ -5,4 +5,16 @@ module.exports = {
   RAZORPAY_API_KEY: process.env.RAZORPAY_API_KEY,
   RAZORPAY_API_SECRET: process.env.RAZORPAY_API_SECRET,
   GOOGLE_APPLICATION_CREDENTIALS: process.env.GOOGLE_APPLICATION_CREDENTIALS,
+  GOOGLE_APPLICATION_CREDENTIALS: {
+    type: process.env.FCM_TYPE,
+    project_id: process.env.FCM_PROJECT_ID,
+    private_key_id: process.env.FCM_PRIVATE_KEY_ID,
+    private_key: process.env.FCM_PRIVATE_KEY,
+    client_email: process.env.FCM_CLIENT_EMAIL,
+    client_id: process.env.FCM_CLIENT_ID,
+    auth_uri: process.env.FCM_AUTH_URI,
+    token_uri: process.env.FCM_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FCM_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.FCM_CLIENT_X509_CERT_URL,
+  },
 };
